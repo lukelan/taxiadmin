9623e7ad5c06653009fc33a6b6a23fe01dac7d13
trong <vuminhtrong>
2012-11-11 00:52:26 +0700

    account create

---
+++ app.js
@@ -0,0 +1,22 @@
+
+/**
+ * Node.js Login Boilerplate
+ * Author : Stephen Braitsch
+ * More Info : http://bit.ly/LsODY8
+ */
+
+var exp = require('express');
+var app = exp.createServer();
+
+app.root = __dirname;
+global.host = 'localhost';
+
+require('./app/config')(app, exp);
+require('./app/server/router')(app);
+
+user = require('./app/server/modules/account-manager');
+app.get('/users', user.findAll);
+
+app.listen(9000, function(){
+	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
---
+++ app/server/modules/usertype-list.js
@@ -0,0 +1,6 @@
+
+module.exports = [
+    {short:"  " , name:"Please select a type"},
+    {short:"DR" , name:"Driver"},
+    {short:"US" , name:"User"},
+]
